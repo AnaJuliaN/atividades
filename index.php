3949bdd59ba452eccb83b7e30cda94e0d75e1649
AnaJuliaN <ana.nunes@aluno.ifsp.edu.br>
2020-02-19 17:27:12 -0300

    organização de atividades

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Teste</title>
+	<link rel="stylesheet" href="css/index.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+    <h1>Página com links das minhas atividades</h1>
+</body>
+</html>
