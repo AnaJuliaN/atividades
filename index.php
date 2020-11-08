fe22fcd672d3c16aaedbe6ca416cb25e853ed5f1
AnaJuliaN <ana.nunes@aluno.ifsp.edu.br>
2020-11-08 18:14:49 -0300

    Atualização 08/11/2020

--- index.php
+++ index.php
@@ -1,20 +1,50 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <title>Teste</title>
-	<link rel="stylesheet" href="css/index.css">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <meta charset="UTF-8">
+    <title>Link para minhas atividades</title>
 </head>
 <body>
-    <h1>Página com links das minhas atividades</h1>
-    <a href='20200811/ex.php'>Atividade 1 |</a>
-    <a href='2020.08.19/index.php'>Atividade 2 |</a>
-    <a href='20200826/index.php'>Atividade 3 |</a>
-    <a href='20200826.individual/exe_ajax.php'>Atividade 4 |</a>
-    <a href='20200916/form.php'>Atividade 5 |</a>
-    <a href='projeto/grupob.php'>Atividade 6 |</a>
+    <h1>Link para minhas atividades:</h1>
+    <p>
+        <h3>Dia 18/08/2020:</h3>
+        <a href="20200818/index.php">Editor de texto com JQuery</a>
+    </p>
+    <p>
+        <h3>Dia 19/08/2020:</h3>
+        <a href="20200819/index.php">Engenharia Reversa</a>
+    </p>
+    <p>
+        <h3>Dia 26/08/2020(atividade individual): </h3>
+        <a href="20200826/index.php"> Atividade conjunta de Ajax com Jquery
+    </p>
+    <p>
+        <h3>Dia 26/08/2020(atividade em grupo): </h3>
+        <a href="AJAXJQUERY/index.php"> Atividade conjunta de Ajax com Jquery
+    </p>
+     <p>
+        <h3>Trabalho em dupla (Christian Galho e José Leonardo) </h3>
+        <a href="trabalho/grupob.php"> Atividade conjunta Jose Leonardo e Christian Galho
+    </p>
+     <p>
+        <h3>Trabalho em grupo (23/09/2020) </h3>
+        <a href="20200923/conexao.php"> Atividade conjunta PHP MySQL
+    </p>
+    <p>
+        <h3>Dia 30/09/2020(atividade em grupo): </h3>
+        <a href="2020.09.30/index.php"> Atividade conjunta
+    </p>
+    <p>
+        <h3>Dia 14/10/2020(atividade em grupo): </h3>
+        <a href="2020.10.14/conexao.php"> Atividade conjunta
+    </p>
+    <p>
+        <h3>Trabalho em dupla 2ºBM (Ana Julia e José Leonardo) </h3>
+        <a href="Ana_Leo_Projeto2bim/index.php"> Atividade conjunta Ana Julia e José Leonardo
+    </p>
+    <p>
+        <h3>Revisão 3ºBimestre - Ana Julia e José Leonardo</h3>
+        <a href="revisao_AnaJ_JoseLeo3Bm/index.php">Revisão 3º Bimestre AnaJ e Joseleo
+    </p>
 </body>
 </html>
