ef4403d7b616e9e7f25e997be34bc70a011e7104
AnaJuliaN <ana.nunes@aluno.ifsp.edu.br>
2020-12-16 18:03:46 -0300

    Atualização 16/12/2020

--- index.php
+++ index.php
@@ -16,39 +16,39 @@
     </p>
     <p>
         <h3>Dia 26/08/2020(atividade individual): </h3>
-        <a href="20200826/index.php"> Atividade conjunta de Ajax com Jquery
+        <a href="20200826/index.php"> Atividade conjunta de Ajax com Jquery</a>
     </p>
     <p>
         <h3>Dia 26/08/2020(atividade em grupo): </h3>
-        <a href="AJAXJQUERY/index.php"> Atividade conjunta de Ajax com Jquery
+        <a href="AJAXJQUERY/index.php"> Atividade conjunta de Ajax com Jquery</a>
     </p>
      <p>
         <h3>Trabalho em dupla (Christian Galho e José Leonardo) </h3>
-        <a href="trabalho/grupob.php"> Atividade conjunta Jose Leonardo e Christian Galho
+        <a href="trabalho/grupob.php"> Atividade conjunta Jose Leonardo e Christian Galho</a>
     </p>
      <p>
         <h3>Trabalho em grupo (23/09/2020) </h3>
-        <a href="20200923/conexao.php"> Atividade conjunta PHP MySQL
+        <a href="20200923/conexao.php"> Atividade conjunta PHP MySQL</a>
     </p>
     <p>
         <h3>Dia 30/09/2020(atividade em grupo): </h3>
-        <a href="2020.09.30/index.php"> Atividade conjunta
+        <a href="2020.09.30/index.php"> Atividade conjunta</a>
     </p>
     <p>
         <h3>Dia 14/10/2020(atividade em grupo): </h3>
-        <a href="2020.10.14/conexao.php"> Atividade conjunta
+        <a href="2020.10.14/conexao.php"> Atividade conjunta</a>
     </p>
     <p>
         <h3>Trabalho em dupla 2ºBM (Ana Julia e José Leonardo) </h3>
-        <a href="Ana_Leo_Projeto2bim/index.php"> Atividade conjunta Ana Julia e José Leonardo
+        <a href="Ana_Leo_Projeto2bim/index.php"> Atividade conjunta Ana Julia e José Leonardo</a>
     </p>
     <p>
         <h3>Revisão 3ºBimestre - Ana Julia e José Leonardo</h3>
-        <a href="revisao_AnaJ_JoseLeo3Bm/index.php">Revisão 3º Bimestre AnaJ e Joseleo
+        <a href="revisao_AnaJ_JoseLeo3Bm/index.php">Revisão 3º Bimestre AnaJ e Joseleo</a>
     </p>
     <p>
         <h3>Projeto Prático 4ºB - Ana Julia e José Leonardo</h3>
-        <a href="Ana.Leo_ProjetoFinal/index.php">Projeto 4º Bimestre AnaJ e Joseleo
+        <a href="Ana.Leo_ProjetoFinal/index.php">Projeto 4º Bimestre AnaJ e Joseleo</a>
     </p>
 </body>
 </html>
